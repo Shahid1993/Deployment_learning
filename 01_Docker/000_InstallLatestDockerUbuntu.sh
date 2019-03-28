029aec21f5702f550affd0a2e9d8bbbb14b70e5c
Shahid1993 <31109285+Shahid1993@users.noreply.github.com>
2019-03-28 14:35:16 +0530

    Update 000_InstallLatestDockerUbuntu.sh

--- 01_Docker/000_InstallLatestDockerUbuntu.sh
+++ 01_Docker/000_InstallLatestDockerUbuntu.sh
@@ -2,3 +2,13 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869
 sudo sh -c "echo deb https://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
 sudo apt-get update
 sudo apt-get install lxc-docker
+
+or ::
+
+#!/bin/bash
+sudo apt-get update
+sudo apt-get -y install linux-image-extra-$(uname -r)
+sudo sh -c "wget -qO- https://get.docker.io/gpg | apt-key add -"
+sudo sh -c "echo deb http://get.docker.io/ubuntu docker main\ > /etc/apt/sources.list.d/docker.list"
+sudo apt-get update
+sudo apt-get -y install lxc-docker
